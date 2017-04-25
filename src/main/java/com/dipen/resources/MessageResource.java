e0bb2ebfb2aa618e02c95086e23040ad3aa85a6c
Dipen Lama <lamadipen@yahoo.com>
2017-04-25 15:00:21 -0500

    Application with layers seperation,url parameters and xml type

--- src/main/java/com/dipen/resources/MessageResource.java
+++ src/main/java/com/dipen/resources/MessageResource.java
@@ -5,6 +5,7 @@ import com.dipen.service.MessageService;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
@@ -18,8 +19,24 @@ public class MessageResource {
     MessageService ms = new MessageService();
     @GET
     @Produces(MediaType.APPLICATION_XML)
-    public List<Message> getMessage()
+    public List<Message> getMessages()
     {
         return ms.getAllMessages();
+    }
+
+    @GET
+    @Path("/{messageId}")
+    @Produces(MediaType.APPLICATION_XML)
+    public Message getMessage(@PathParam("messageId") long messageId)
+    {
+        return ms.getMessage(messageId);
+    }
+
+    @GET
+    @Path("test")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String test()
+    {
+        return "Method path test";
     }
 }
--- src/main/java/com/dipen/service/MessageService.java
+++ src/main/java/com/dipen/service/MessageService.java
@@ -1,20 +1,54 @@
 package com.dipen.service;
 
+import com.dipen.dao.DatabaseClass;
 import com.dipen.model.Message;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by dipen on 4/25/2017.
  */
 public class MessageService {
 
+    private  Map<Long,Message> messages = DatabaseClass.getAllMessages();
+
+    public  MessageService()
+    {
+        messages.put(1l,new Message(1,"Hello Dipen","Dilip"));
+        messages.put(2l,new Message(2,"Hello Dipen","Dilip"));
+
+    }
     public List<Message> getAllMessages()
     {
-        List<Message> list = new ArrayList<>();
-        list.add(new Message(1,"Hello Dipen","Dilip"));
-        list.add(new Message(1,"Hello Suman","Dipen"));
-        return list;
+        return new ArrayList<Message>(messages.values());
+    }
+
+    public Message getMessage(long id)
+    {
+        return messages.get(id);
+    }
+
+    public Message addMessage(Message message)
+    {
+        message.setId(messages.size()+1);
+        messages.put(message.getId(),message);
+        return message;
+    }
+
+    public Message updateMessage(Message message)
+    {
+        if(message.getId() < 0)
+        {
+            return null;
+        }
+        messages.put(message.getId(),message);
+        return message;
+    }
+
+    public Message removeMessage(long id)
+    {
+        return messages.remove(id);
     }
 }
